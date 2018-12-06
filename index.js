85dc54fe3008345891a4a9cc03d6120593285842
Magnus Trandokken <magnus@magson.no>
2018-12-06 19:23:56 +0100

    Fixed for ullernrevyen

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-// Set the date we're counting down to
-var countDownDate = new Date("May 21, 2019 00:00:00").getTime();
+var countDownDate = new Date("December 22, 2018 00:00:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function () {
-
-    // Get todays date and time
     var now = new Date().getTime();
-
-    // Find the distance between now an the count down date
     var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in an element with id="demo"
     document.getElementById("counter").innerHTML = days + "d " + hours + "t "
         + minutes + "m " + seconds + "s ";
+    document.getElementById('counter').style.opacity = 1; 
 
-    // If the count down is finished, write some text 
+
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("counter").innerHTML = "EXPIRED";
     }
 }, 1000);
+
+var i = 0;
+var txt = 'Ullernrevyen 2019';
+var speed = 70; //Duration
+
+var typeWriter = setInterval(() => {
+    if (i < txt.length) {
+        document.getElementById("title").innerHTML += txt.charAt(i);
+        i++;
+    }
+}, speed);
+
+ScrollReveal().reveal('.test');
